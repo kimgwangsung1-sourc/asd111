9a9076079840548587383452fe5099ccf738aa55
kimgwangsung1 <kimgwangsung1@gmail.com>
2026-03-17 02:50:19 +0000

    Fix privacy page redirect loop

--- functions/privacy/index.js
+++
@@ -1,6 +0,0 @@
-export function onRequest(context) {
-  const assetUrl = new URL("/privacy-static.html", context.request.url);
-  const assetRequest = new Request(assetUrl.toString(), context.request);
-
-  return context.env.ASSETS.fetch(assetRequest);
-}
